141199d2036f0d46f5a9df2ab2c2ed423b8b2d82
liyiru <liyr@ehualu.com>
2022-05-04 22:23:33 -0400

    version 2.0

--- run_clf_roberta.py
+++ run_clf_roberta.py
@@ -2,7 +2,7 @@
 
 '''Setups'''
 import os
-import zipfile
+# import zipfile
 import torch
 import numpy as np
 import pandas as pd
@@ -11,6 +11,8 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trai
 
 from senti import SentiDataset
 from utils import compute_metrics
+
+print("code start")
 
 # GPU detection
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -27,12 +29,10 @@ os.environ["WORLD_SIZE"] = "1"
 !wget https://raw.githubusercontent.com/huggingface/transformers/main/tests/deepspeed/ds_config_zero3.json
 
 
-
-'''
 with zipfile.ZipFile("/scratch/yl5865/ds301_final_project/trainingandtestdata.zip","r") as zip_ref:
     zip_ref.extractall("/scratch/yl5865/ds301_final_project/data/")
 
-'''Read-in the Training Tweets'''
+# Read-in the Training Tweets
 raw_data_df = pd.read_csv("/scratch/yl5865/ds301_final_project/data/training.1600000.processed.noemoticon.csv",encoding="ISO-8859-1")
 raw_data_df.columns = ["labels","ids","date","query","user","tweets"]
 data_df = raw_data_df.drop(["ids","date","query","user"],axis=1)
@@ -48,14 +48,36 @@ data_df = data_df.sample(TOTAL, random_state=7)
 train_data_df = data_df.iloc[:int(TOTAL*(1-TTSPLIT_FACTOR)),:]
 val_data_df = data_df.iloc[-int(TOTAL*(1-TTSPLIT_FACTOR)):,:]
 
-
-'''Prepare the Dataset'''
 tokenizer = AutoTokenizer.from_pretrained("roberta-base")
 # small_train_data = SentiDataset(small_train_data_df,tokenizer)
 # small_val_data = SentiDataset(small_val_data_df,tokenizer)
 train_data = SentiDataset(train_data_df,tokenizer)
 val_data = SentiDataset(val_data_df,tokenizer)
+'''
+from datasets import load_dataset
+dataset = load_dataset("sentiment140")
 
+print("data loaded")
+
+tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+print("right after tokenize")
+data = tokenized_datasets["train"]
+data = data.map(lambda example: {'labels':1} if example['sentiment']==4 else {"labels":0}, remove_columns=["date","user","query","sentiment","text"])
+SPLIT_FACTOR = 0.85
+TOTAL_TRAIN = len(data)
+
+data = data.shuffle(seed=7)
+
+train_dataset = data.select(range(0,int(TOTAL_TRAIN*SPLIT_FACTOR)))
+eval_dataset = data.select(range(int(TOTAL_TRAIN*SPLIT_FACTOR),TOTAL_TRAIN))
+
+print("right before Classifier")
 
 '''Train the Classifier'''
 model = AutoModelForSequenceClassification.from_pretrained("roberta-base", num_labels=2).to(device)
@@ -74,15 +96,19 @@ else:
   trainArgs = TrainingArguments(evaluation_strategy="epoch",
                           save_strategy="epoch",
                           num_train_epochs=3,
+                          per_device_train_batch_size=32,
+                          per_device_eval_batch_size=16,
                           load_best_model_at_end=True,
                           output_dir="/scratch/yl5865/ds301_final_project/model",
                           deepspeed="/scratch/yl5865/ds301_final_project/configs/ds_config_zero3.json")
 
 trainer = Trainer(model=model,
                   args=trainArgs,
-                  train_dataset=train_data,
-                  eval_dataset=val_data,
+                  train_dataset=train_dataset,
+                  eval_dataset=eval_dataset,
                   compute_metrics=compute_metrics)
 
+print("right before trainer started")
+
 trainer.train()
-trainer.save_model('/scratch/yl5865/ds301_final_project/tuned_roberta_model')
+trainer.save_pretrained('/scratch/yl5865/ds301_final_project/tuned_roberta_model')
